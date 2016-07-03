6378c224ef4a6aecce7eaef75bb91b2335e26836
Oleh Smolynets <smolynets@gmail.com>
2016-07-03 18:37:27 +0300

    add mysql  to settings.py

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -73,7 +73,27 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
+    # }
+   # 'default': {
+    #    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     #   'NAME': 'mydb1',
+      #  'USER': 'oleh1',
+       # 'PASSWORD': '0000',
+       # 'HOST': 'localhost',
+       # 'PORT': '',
+    #}
+    'default': {
+      'ENGINE': 'django.db.backends.mysql',
+      'HOST': 'localhost',
+      'USER': 'students_db_user',
+      'PASSWORD': '0000',
+      'NAME': 'students_db',
+    }
+}
 
 
 
